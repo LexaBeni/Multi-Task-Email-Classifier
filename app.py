c3dea218b2301f6ff2c46913887e9edfcad53943
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-06-23 16:52:42 +0200

    Completed app.py

--- app.py
+++ app.py
@@ -5,14 +5,28 @@ import torch
 from transformers import AutoTokenizer
 from src.architecture import MultiTaskEmailModel
 import streamlit as st
+import time
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+st.markdown("""
+This application uses a multi-task BERT model to predict:
 
-model = MultiTaskEmailModel()
+- Email Category
+- Priority
+- Intent
+""")
 
-model.load_state_dict(torch.load('D:\IT\Python\pytorch\email_classification_project\models\multitask_model.pt', map_location = 'cpu'))
+@st.cache_resource
+def load_model_and_tokenizer():
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    model = MultiTaskEmailModel()
+    
+    model_path = r'D:\IT\Python\pytorch\email_classification_project\models\multitask_model.pt'
+    
+    model.load_state_dict(torch.load(model_path, map_location='cpu'))
+    model.eval()
+    return model, tokenizer
 
-model.eval()
+model, tokenizer = load_model_and_tokenizer()
 
 def predict_text(text):
     tokens = tokenizer(text, truncation = True, max_length = 64, padding=True, return_tensors = 'pt')
@@ -56,34 +70,41 @@ intent_map = {
 }
 
 st.title("📧 Email Classifier")
-text = st.text_area("Enter email text")
+if "email" not in st.session_state:
+    st.session_state['email'] = ""
 samples = st.radio("Samples", ["We need to deploy the new database schema by 5 PM today. Please approve the release configuration.", "Congratulations! You won a $1000 cash prize. Claim your financial reward here: http://claim-reward-now.net", "Security notification: A new sign-in was detected on your production server from a new device."], index=None)
-analyze = st.button("Analyze")
-if samples is not None:
-    text = samples
+if samples:
+    st.session_state.email = samples
+email = st.text_area("Enter email text", value=st.session_state.email, height=150)
+analyze = st.button("Analyze")   
 if analyze:
-    spam, priority, intent, spam_confidence, priority_confidence, intent_confidence = predict_text(text)
+    with st.spinner("Analyzing email..."):
+        time.sleep(2)
+    spam, priority, intent, spam_confidence, priority_confidence, intent_confidence = predict_text(email)
 
     st.success("Analysis complete")
-
-    st.write(
-        f"Category: {spam_map[spam]}, chance {spam_confidence}"
-    )
-    if priority == 2:
-        st.error(
-            f"Priority: {priority_map[priority]}, chance {priority_confidence}"
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write(
+            f"Category: {spam_map[spam]}, chance {spam_confidence:.1%}"
         )
-    elif priority == 1:
-        st.warning(
-            f"Priority: {priority_map[priority]}, chance {priority_confidence}"
+    with col2:
+        if priority == 2:
+            st.error(
+                f"Priority: {priority_map[priority]}, chance {priority_confidence:.1%}"
+            )
+        elif priority == 1:
+            st.warning(
+                f"Priority: {priority_map[priority]}, chance {priority_confidence:.1%}"
+            )
+        else:
+            st.success(
+                f"Priority: {priority_map[priority]}, chance {priority_confidence:.1%}"
+            )
+    with col3:
+        st.write(
+            f"Intent: {intent_map[intent]}, chance {intent_confidence:.1%}"
         )
-    else:
-        st.success(
-            f"Priority: {priority_map[priority]}, chance {priority_confidence}"
-        )
-    st.write(
-        f"Intent: {intent_map[intent]}, chance {intent_confidence}"
-    )
 
 
 
